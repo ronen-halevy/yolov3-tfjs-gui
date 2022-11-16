2f081877ca9feaf2440e5b6c8f31d6e531987736
ronen-halevy <ronen567@gmail.com>
2022-11-16 15:02:59 +0200

    starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
 
-function App() {
+const App = () => {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const stripRef = useRef(null);
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error("error:", err);
+      });
+  };
+
+  const paintToCanvas = () => {
+    let video = videoRef.current;
+    let photo = photoRef.current;
+    let ctx = photo.getContext("2d");
+
+    const width = 320;
+    const height = 240;
+    photo.width = width;
+    photo.height = height;
+
+    return setInterval(() => {
+      ctx.drawImage(video, 0, 0, width, height);
+    }, 200);
+  };
+
+  const takePhoto = () => {
+    let photo = photoRef.current;
+    let strip = stripRef.current;
+
+    console.warn(strip);
+
+    const data = photo.toDataURL("image/jpeg");
+
+    console.warn(data);
+    const link = document.createElement("a");
+    link.href = data;
+    link.setAttribute("download", "myWebcam");
+    link.innerHTML = `<img src='${data}' alt='thumbnail'/>`;
+    strip.insertBefore(link, strip.firstChild);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => takePhoto()}>Take a photo</button>
+      <video onCanPlay={() => paintToCanvas()} ref={videoRef} />
+      <canvas ref={photoRef} />
+      <div>
+        <div ref={stripRef} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
