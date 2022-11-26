044398970f9c14dd0ed9a14c5cbc694399dd4f7f
ronen-halevy <ronen567@gmail.com>
2022-11-26 23:25:58 +0200

    auto cosmetics - nothing really

--- src/components/LoadModel.js
+++ src/components/LoadModel.js
@@ -1,17 +1,17 @@
-import * as tf from "@tensorflow/tfjs";
-import { loadGraphModel } from "@tensorflow/tfjs-converter";
+import * as tf from '@tensorflow/tfjs';
+import { loadGraphModel } from '@tensorflow/tfjs-converter';
 
 async function LoadModel() {
-  // It's possible to load the model locally or from a repo
-  // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
-  //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-  // const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
+	// It's possible to load the model locally or from a repo
+	// You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
+	//const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
+	// const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
 
-  const MODEL_URL = "http://127.0.0.1:8887/models/shapes/model.json";
-  const model = await tf.loadLayersModel(MODEL_URL);
-  //console.log(model.summary());
+	const MODEL_URL = 'http://127.0.0.1:8887/models/shapes/model.json';
+	const model = await tf.loadLayersModel(MODEL_URL);
+	//console.log(model.summary());
 
-  return model;
+	return model;
 }
 
 // async function LoadModel() {
