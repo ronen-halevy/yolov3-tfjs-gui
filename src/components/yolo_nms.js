df0a63ceba689021400c7e402ec790ed1754369a
ronen-halevy <ronen567@gmail.com>
2022-11-30 00:12:11 +0200

    wip extract nms

--- src/components/yolo_nms.js
+++ src/components/yolo_nms.js
@@ -1,48 +1,44 @@
-import * as tf from "@tensorflow/tfjs";
+import * as tf from '@tensorflow/tfjs';
+const yoloNms = (
+	bboxes,
+	scores,
+	yoloMaxBoxes,
+	nmsIouThreshold,
+	nmsScoreThreshold
+) => {
+	// let axis = -1;
+	// let classIndices = classProbs.argMax(axis);
 
-async function yolo_nms(
-  bboxes,
-  confidence,
-  classProbs,
-  yolo_max_boxes,
-  nms_iou_threshold,
-  nms_score_threshold
-) {
-  // let axis = 0;
-  // let [selectedBboxes, selectedScores, selectedClasses] = [0, 0, 0];
-  // return [selectedBboxes, selectedScores, selectedClasses];
-  // bboxes = bboxes.squeeze(axis);
-  // confidence = confidence.squeeze(axis);
-  // let classProbs = class_probs.squeeze(axis);
-  axis = -1;
-  // pre each bbox, select class with max prob:
-  // let classIndices = classProbs.argMax(axis);
-  // select class from class probs array
+	// classProbs = classProbs.max(axis);
+	// confidences = confidences.squeeze(axis);
+	// let scores = confidences.mul(classProbs);
 
-  classProbs = classProbs.max(axis);
-  confidence = confidence.squeeze(axis);
-  let scores = confidence.mul(classProbs);
+	const nms = new Promise((resolve) => {
+		const nmsResults = tf.image.nonMaxSuppressionAsync(
+			bboxes,
+			scores,
+			yoloMaxBoxes,
+			nmsIouThreshold,
+			nmsScoreThreshold
+		);
+		resolve(nmsResults);
+	});
 
-  // non_max_suppression_padded vs non_max_suppression supports batched input, returns results per batch
-  //const pad_to_max_output_size = true;
-  let nmsResults = tf.image.nonMaxSuppressionAsync(
-    bboxes,
-    scores,
-    yolo_max_boxes,
-    nms_iou_threshold,
-    nms_score_threshold
-  );
-  //nmsResults.print();
+	nms.then((nmsResults) => {
+		let selectedBboxes = bboxes.gather(nmsResults);
+		let selectedClasses = classIndices.gather(nmsResults);
+		let selectedScores = scores.gather(nmsResults);
 
-  //bboxes.gather(nmsResults).print(); // todo clean
-  //classIndices.gather(nmsResults).print();
-  // scores.gather(nmsResults).print();
-  return nmsResults;
+		const bboxesArray = selectedBboxes.array();
+		const scoresArray = selectedScores.array();
+		const classIndicesArray = selectedClasses.array();
+		let reasultArrays = Promise.all([
+			bboxesArray,
+			scoresArray,
+			classIndicesArray,
+		]);
+		return reasultArrays;
+	});
+};
 
-  // let selectedBboxes = bboxes.gather(nmsResults);
-  // let selectedClasses = classIndices.gather(nmsResults);
-  // let selectedScores = scores.gather(nmsResults);
-
-  return [selectedBboxes, selectedScores, selectedClasses];
-}
-export default yolo_nms;
+export default yoloNms;
