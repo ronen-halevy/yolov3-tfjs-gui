7354e1d682fe2f6c559c4454ec26d892d769c5f9
ronen-halevy <ronen567@gmail.com>
2022-12-04 23:57:20 +0200

    remove from components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import Detection from "./components/Detection.js";
-import YoloV3 from "./components/YoloV3.js";
+import YoloV3 from './YoloV3.js';
 
 const App = () => {
-  return (
-    <div>
-      <YoloV3 />
-    </div>
-  );
+	return (
+		<div>
+			<YoloV3 />
+		</div>
+	);
 };
 
 export default App;
