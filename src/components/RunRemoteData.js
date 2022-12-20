b108ff8036f1bf24d7a1f86e44dabab4c6c1cd94
ronen-halevy <ronen567@gmail.com>
2022-12-21 00:38:04 +0200

    place badges

--- src/components/RunRemoteData.js
+++ src/components/RunRemoteData.js
@@ -14,8 +14,10 @@ class RunRemoteData extends React.Component {
                     Select an Example
                   </label> */}
           </div>
-          <div className='col position-relative'>
-            <span class='  badge rounded-pill  bg-primary'>Select</span>
+          <div className='col position-relative mt-2'>
+            <span class='position-absolute top-10  start-50 translate-middle badge rounded-pill bg-primary'>
+              Select
+            </span>
 
             <select
               className='form-select form-select-lg mb-1'
@@ -27,12 +29,12 @@ class RunRemoteData extends React.Component {
                 </option>
               ))}
             </select>
-            <span class='  position-absolute top-0  start-50 translate-middle badge rounded-pill bg-secondary'>
+            <span class='  position-absolute  start-50 translate-middle badge rounded-pill bg-secondary mb-1'>
               Credit: Videos are fetched from https://mixkit.co/
             </span>
           </div>
         </div>
-        <div className='row '>
+        <div className='row mt-5'>
           <RunButton
             onClickRunRemote={this.props.onClickRunRemote}
             isVideoOn={this.props.isVideoOn}
