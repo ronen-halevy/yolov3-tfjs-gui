6dfc191ae765a117b960363e80444cfeee58fda7
ronen-halevy <ronen567@gmail.com>
2022-12-22 00:00:13 +0200

    +buttons

--- src/components/RunButton.js
+++ src/components/RunButton.js
@@ -14,14 +14,14 @@ class RunButton extends React.Component {
         {!this.props.isVideoOn ? (
           <div>
             play{' '}
-            <span className='position-absolute top-0  start-50 translate-middle badge rounded-pill bg-primary'>
+            <span className='position-absolute top-0  start-50 translate-middle badge rounded-pill bg-success'>
               {this.props.badgeLabel}
             </span>{' '}
           </div>
         ) : (
           <div>
             Stop{' '}
-            <span className='position-absolute top-0  start-50 translate-middle badge rounded-pill bg-success'>
+            <span className='position-absolute top-0  start-50 translate-middle badge rounded-pill bg-danger'>
               Running
             </span>
           </div>
