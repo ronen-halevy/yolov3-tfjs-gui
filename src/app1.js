8475a0d939e906b57075a689e62e76d600db1fdf
ronen-halevy <ronen567@gmail.com>
2023-01-10 21:40:35 +0200

    with math

---
+++ src/app1.js
@@ -0,0 +1,9 @@
+export const app = () => {
+  const { PI, square } = require('./math');
+
+  //   const cats = require('./shelter');
+  console.log(PI);
+
+  console.log(square(9));
+  //   console.log('REQUIRED AN ENTIRE DIRECTORY!', cats);
+};
