6ae66884d042f28f44e3de3476b297372003e4d5
ronen-halevy <ronen567@gmail.com>
2023-01-10 23:39:09 +0200

    math wip

--- src/math.js
+++ src/math.js
@@ -2,14 +2,9 @@ const add = (x, y) => x + y;
 
 const PI = 3.14159;
 
-const square = x => x * x;
-
-exports.square = square;
-exports.PI = PI;
+const square = (x) => x * x;
 
 // =========================
-// module.exports.add = add;
-// module.exports.PI = PI;
-// module.exports.square = square;
-
-
+module.exports.add = add;
+module.exports.PI = PI;
+module.exports.square = square;
