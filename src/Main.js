3bd405ee5eaeb8e154f9aab1abd0eb74a057087f
ronen-halevy <ronen567@gmail.com>
2023-06-19 21:09:50 +0300

    use yolo cdn module

--- src/Main.js
+++ src/Main.js
@@ -7,115 +7,116 @@ import { VideoControlPanel } from './components/VideoControlPanel';
 
 import configNms from './config/configNms.json';
 
-import { YoloV3 } from './yolov3/YoloV3.js';
+// import cdn module:
+// import { YoloV3 } from './yolov3/YoloV3.js';
 
 import Render from './utils/Render.js';
 export class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.title = ''; // vid/img display title - currently unused
-    this.dataUrl = '';
-    this.dataType = '';
-    this.state = {
-      classNames: '',
-    };
-    this.yoloCreated = false;
-  }
+	constructor(props) {
+		super(props);
+		this.title = ''; // vid/img display title - currently unused
+		this.dataUrl = '';
+		this.dataType = '';
+		this.state = {
+			classNames: '',
+		};
+		this.yoloCreated = false;
+	}
 
-  detectFrame = (frame) => {
-    return this.yoloV3.detectFrame(frame);
-  };
+	detectFrame = (frame) => {
+		return this.yoloV3.detectFrame(frame);
+	};
 
-  onLoadModel = (model, anchors, classNames) => {
-    if (!this.yoloCreated) {
-      // note: configNms values are expected to be overrided by config component on init:
-      this.yoloV3 = new YoloV3(
-        model,
-        anchors,
-        classNames.length,
-        configNms.scoreTHR,
-        configNms.iouTHR,
-        configNms.maxBoxes
-      );
-      this.yoloCreated = true;
-    } else {
-      this.yoloV3.setModelParams(model, anchors, classNames.length);
-    }
+	onLoadModel = (model, anchors, classNames) => {
+		if (!this.yoloCreated) {
+			// note: configNms values are expected to be overrided by config component on init:
+			this.yoloV3 = new YoloV3(
+				model,
+				anchors,
+				classNames.length,
+				configNms.scoreTHR,
+				configNms.iouTHR,
+				configNms.maxBoxes
+			);
+			this.yoloCreated = true;
+		} else {
+			this.yoloV3.setModelParams(model, anchors, classNames.length);
+		}
 
-    this.setState({ classNames: classNames });
-  };
+		this.setState({ classNames: classNames });
+	};
 
-  setScoreTHR = (val) => {
-    this.yoloV3.setScoreTHR(val);
-  };
-  setIouTHR = (val) => {
-    this.yoloV3.setIouTHR(val);
-  };
-  setMaxBoxes = (val) => {
-    this.yoloV3.setMaxBoxes(val);
-  };
+	setScoreTHR = (val) => {
+		this.yoloV3.setScoreTHR(val);
+	};
+	setIouTHR = (val) => {
+		this.yoloV3.setIouTHR(val);
+	};
+	setMaxBoxes = (val) => {
+		this.yoloV3.setMaxBoxes(val);
+	};
 
-  onClickSetDataSource = (url, type, title) => {
-    this.setState({ title: title });
-    this.setState({ dataUrl: url });
-    this.dataUrl = url;
-    this.dataType = type;
-  };
+	onClickSetDataSource = (url, type, title) => {
+		this.setState({ title: title });
+		this.setState({ dataUrl: url });
+		this.dataUrl = url;
+		this.dataType = type;
+	};
 
-  render() {
-    const {} = this.props;
-    return (
-      <div className='container '>
-        <h2 className='text-center mb-1 mt-2'>Yolo TfJs Demo</h2>
-        <Accordion />
+	render() {
+		const {} = this.props;
+		return (
+			<div className='container '>
+				<h2 className='text-center mb-1 mt-2'>Yolo TfJs Demo</h2>
+				<Accordion />
 
-        <div className='col '>
-          <div className=' row text-center'>
-            <div className=' col'>Model Selection</div>
-          </div>
-          <div className='model  border border-1 border-secondary position-relative bg-light'>
-            <ModelSelectionPanel onLoadModel={this.onLoadModel} />
-          </div>
+				<div className='col '>
+					<div className=' row text-center'>
+						<div className=' col'>Model Selection</div>
+					</div>
+					<div className='model  border border-1 border-secondary position-relative bg-light'>
+						<ModelSelectionPanel onLoadModel={this.onLoadModel} />
+					</div>
 
-          <div className=' row text-center'>
-            <div className=' col'>Data Source Selection </div>
-          </div>
+					<div className=' row text-center'>
+						<div className=' col'>Data Source Selection </div>
+					</div>
 
-          <div className='dataSource border border-1 border-secondary position-relative bg-light'>
-            <DataSourceSelectionPanel
-              onClickSetDataSource={this.onClickSetDataSource}
-            />
-          </div>
+					<div className='dataSource border border-1 border-secondary position-relative bg-light'>
+						<DataSourceSelectionPanel
+							onClickSetDataSource={this.onClickSetDataSource}
+						/>
+					</div>
 
-          <div className=' row text-center'>
-            <div className=' col'>Configurations </div>
-          </div>
-          <div className='configButtons border border-1 border-secondary position-relative  bg-light'>
-            <div className='row mb-2'>
-              {/* send configs updates not before object is constructed */}
-              {this.yoloCreated && (
-                <ConfigurationsPanel
-                  setScoreTHR={this.setScoreTHR}
-                  setIouTHR={this.setIouTHR}
-                  setMaxBoxes={this.setMaxBoxes}
-                />
-              )}
-            </div>
-          </div>
-        </div>
-        <div className=' row text-center'>
-          <div className=' col'>Video Control</div>
-        </div>
-        <div className='col  border border-1 border-secondary bg-light '>
-          <div className='row mb-2'>
-            <VideoControlPanel
-              detectFrame={this.detectFrame}
-              classNames={this.state.classNames}
-              inputUrl={{ url: this.dataUrl, type: this.dataType }}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+					<div className=' row text-center'>
+						<div className=' col'>Configurations </div>
+					</div>
+					<div className='configButtons border border-1 border-secondary position-relative  bg-light'>
+						<div className='row mb-2'>
+							{/* send configs updates not before object is constructed */}
+							{this.yoloCreated && (
+								<ConfigurationsPanel
+									setScoreTHR={this.setScoreTHR}
+									setIouTHR={this.setIouTHR}
+									setMaxBoxes={this.setMaxBoxes}
+								/>
+							)}
+						</div>
+					</div>
+				</div>
+				<div className=' row text-center'>
+					<div className=' col'>Video Control</div>
+				</div>
+				<div className='col  border border-1 border-secondary bg-light '>
+					<div className='row mb-2'>
+						<VideoControlPanel
+							detectFrame={this.detectFrame}
+							classNames={this.state.classNames}
+							inputUrl={{ url: this.dataUrl, type: this.dataType }}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
--- src/components/ModelSelectionPanel.js
+++ src/components/ModelSelectionPanel.js
@@ -3,87 +3,89 @@ import React, { Component } from 'react';
 import SelectModelButtons from './SelectModelButtons';
 
 import configModel from '../config/configModel.json';
-import { createModel } from '../yolov3/YoloV3.js';
+
+// import cdn module:
+//import { createModel } from '../yolov3/YoloV3.js';
 
 export default class ModelSelectionPanel extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      loadedModel: '',
-      loadedWeights: '',
-      loadingMessage: 'No Model Loaded!',
-      loadSpinner: false,
-    };
-    this.modelsTable = configModel.models;
-    // take first in list as a default:
-    this.selectedModel = Object.keys(this.modelsTable)[0];
-    this.selectedWeights = Object.keys(this.modelsTable[this.selectedModel])[0];
-  }
+		this.state = {
+			loadedModel: '',
+			loadedWeights: '',
+			loadingMessage: 'No Model Loaded!',
+			loadSpinner: false,
+		};
+		this.modelsTable = configModel.models;
+		// take first in list as a default:
+		this.selectedModel = Object.keys(this.modelsTable)[0];
+		this.selectedWeights = Object.keys(this.modelsTable[this.selectedModel])[0];
+	}
 
-  componentDidMount() {
-    this.onLoadModel();
-  }
+	componentDidMount() {
+		this.onLoadModel();
+	}
 
-  setModelAndWeights = (results) => {
-    const { selectedModel, selectedWeights } = results;
-    // this.setState({
-    console.log(this.selectedModel, this.selectedWeights);
+	setModelAndWeights = (results) => {
+		const { selectedModel, selectedWeights } = results;
+		// this.setState({
+		console.log(this.selectedModel, this.selectedWeights);
 
-    this.selectedModel = selectedModel;
-    this.selectedWeights = selectedWeights;
-  };
+		this.selectedModel = selectedModel;
+		this.selectedWeights = selectedWeights;
+	};
 
-  onLoadModel = () => {
-    this.setState({ loadingMessage: 'Loading Model...', loadSpinner: true });
+	onLoadModel = () => {
+		this.setState({ loadingMessage: 'Loading Model...', loadSpinner: true });
 
-    const modelConfig =
-      this.modelsTable[this.selectedModel][this.selectedWeights];
-    const { modelUrl, anchorsUrl, classNamesUrl, ...rest } = modelConfig;
+		const modelConfig =
+			this.modelsTable[this.selectedModel][this.selectedWeights];
+		const { modelUrl, anchorsUrl, classNamesUrl, ...rest } = modelConfig;
 
-    createModel(modelUrl, anchorsUrl, classNamesUrl).then((res) => {
-      this.setState({
-        loadedModel: this.selectedModel,
-        loadedWeights: this.selectedWeights,
+		createModel(modelUrl, anchorsUrl, classNamesUrl).then((res) => {
+			this.setState({
+				loadedModel: this.selectedModel,
+				loadedWeights: this.selectedWeights,
 
-        loadingMessage:
-          this.selectedModel + ' + ' + this.selectedWeights + ' is ready!',
-        loadSpinner: false,
-      });
-      this.model = res[0];
-      this.anchors = res[1].anchor;
-      this.classNames = res[2].split(/\r?\n/);
-      this.nclasses = this.classNames.length;
-      this.props.onLoadModel(this.model, this.anchors, this.classNames);
-    });
-  };
+				loadingMessage:
+					this.selectedModel + ' + ' + this.selectedWeights + ' is ready!',
+				loadSpinner: false,
+			});
+			this.model = res[0];
+			this.anchors = res[1].anchor;
+			this.classNames = res[2].split(/\r?\n/);
+			this.nclasses = this.classNames.length;
+			this.props.onLoadModel(this.model, this.anchors, this.classNames);
+		});
+	};
 
-  render() {
-    return (
-      <div>
-        <div className='selectModelAndDataset row mt-2'>
-          <div className='col-4  text-center '>
-            <span
-              className='btn btn-dark btn-lg  position-relative badge start-0'
-              onClick={this.onLoadModel}
-            >
-              Load
-              <span className='position-absolute top-0  start-50 translate-middle badge rounded-pill bg-danger'>
-                {this.state.loadedModel}+{this.state.loadedWeights}
-              </span>
-              {this.state.loadSpinner && (
-                <div className='spinner-border' role='status'>
-                  <span className='sr-only'></span>
-                </div>
-              )}
-            </span>
-          </div>
-          <SelectModelButtons
-            modelsTable={this.modelsTable}
-            setModelAndWeights={this.setModelAndWeights}
-          />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div className='selectModelAndDataset row mt-2'>
+					<div className='col-4  text-center '>
+						<span
+							className='btn btn-dark btn-lg  position-relative badge start-0'
+							onClick={this.onLoadModel}
+						>
+							Load
+							<span className='position-absolute top-0  start-50 translate-middle badge rounded-pill bg-danger'>
+								{this.state.loadedModel}+{this.state.loadedWeights}
+							</span>
+							{this.state.loadSpinner && (
+								<div className='spinner-border' role='status'>
+									<span className='sr-only'></span>
+								</div>
+							)}
+						</span>
+					</div>
+					<SelectModelButtons
+						modelsTable={this.modelsTable}
+						setModelAndWeights={this.setModelAndWeights}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
